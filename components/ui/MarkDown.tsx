98f83a4388459ae70e939815483cefab57c31ee5
Daniel Sasse <dsasse07@gmail.com>
2021-07-27 08:10:32 -0400

    added style components to Markdown

--- components/ui/MarkDown.tsx
+++ components/ui/MarkDown.tsx
@@ -8,35 +8,54 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import {
   darcula,
   materialLight,
+  materialOceanic,
 } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import React, { ReactNode } from 'react'
+import React from 'react'
+import { useBreakpoint } from '../../utils/useBreakpointProvider'
+import styled from 'styled-components'
 
 interface MarkdownProps {
   children: string
 }
 
-export const CodeBlock: Partial<NormalComponents & SpecialComponents> = {
-  code({ node, inline, className, children, ...props }) {
-    const match = /language-(\w+)/.exec(className || '')
-    return !inline && match ? (
-      <SyntaxHighlighter
-        style={darcula}
-        language={match[1]}
-        PreTag='div'
-        children={String(children).replace(/\n$/, '')}
-        {...props}
-      />
-    ) : (
-      <code className={className} {...props}>
-        {children}
-      </code>
-    )
-  },
-}
-
 const MarkDown: React.FC<MarkdownProps> = ({ children }) => {
-  console.log(CodeBlock)
-  return <ReactMarkdown components={CodeBlock}>{children}</ReactMarkdown>
+  const breakpoint = useBreakpoint()
+
+  const CodeBlock: Partial<NormalComponents & SpecialComponents> = {
+    code({ node, inline, className, children, ...props }) {
+      const match = /language-(\w+)/.exec(className || '')
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={breakpoint.dark ? darcula : materialLight}
+          language={match[1]}
+          PreTag='div'
+          children={String(children).replace(/\n$/, '')}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      )
+    },
+  }
+
+  return (
+    <MarkDownContainer components={CodeBlock}>{children}</MarkDownContainer>
+  )
 }
 
 export default MarkDown
+
+const MarkDownContainer = styled(ReactMarkdown)`
+  display: flex;
+  margin: 0 auto;
+  flex-direction: column;
+  max-width: 70vw;
+
+  pre {
+    margin: 0 auto;
+    max-width: 800px;
+    width: 100%;
+  }
+`
